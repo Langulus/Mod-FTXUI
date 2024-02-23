1540b76cc96035930aa21d65390ffca538a309ee
Dimo Markov <dimomarkov@gmail.com>
2024-02-24 01:25:36 +0200

    Improved test leak detection

--- test/TestGUI.cpp
+++ test/TestGUI.cpp
@@ -10,27 +10,35 @@
 #include <Entity/Thing.hpp>
 #include <catch2/catch.hpp>
 
+
 /// See https://github.com/catchorg/Catch2/blob/devel/docs/tostring.md        
 CATCH_TRANSLATE_EXCEPTION(::Langulus::Exception const& ex) {
    const Text serialized {ex};
    return ::std::string {Token {serialized}};
 }
 
-
 SCENARIO("GUI creation", "[gui]") {
-   Allocator::State memoryState;
+   static Allocator::State memoryState;
    
    for (int repeat = 0; repeat != 10; ++repeat) {
       GIVEN(std::string("Init and shutdown cycle #") + std::to_string(repeat)) {
          // Create root entity                                          
          Thing root;
          root.SetName("ROOT");
-
-         // Create runtime at the root                                  
          root.CreateRuntime();
-
-         // Load FTXUI module                                           
          root.LoadMod("FTXUI");
+
+         WHEN("The GUI system is created via abstractions") {
+            auto gui = root.CreateUnit<A::UI::System>();
+
+            // Update once                                              
+            root.Update(Time::zero());
+            root.DumpHierarchy();
+
+            REQUIRE(gui.GetCount() == 1);
+            REQUIRE(gui.CastsTo<A::UI::System>(1));
+            REQUIRE(gui.IsSparse());
+         }
 
       #if LANGULUS_FEATURE(MANAGED_REFLECTION)
          WHEN("The GUI system is created via tokens") {
@@ -38,31 +46,13 @@ SCENARIO("GUI creation", "[gui]") {
 
             // Update once                                              
             root.Update(Time::zero());
+            root.DumpHierarchy();
 
-            THEN("Various traits change") {
-               root.DumpHierarchy();
-
-               REQUIRE(gui.GetCount() == 1);
-               REQUIRE(gui.CastsTo<A::UI::System>(1));
-               REQUIRE(gui.IsSparse());
-            }
+            REQUIRE(gui.GetCount() == 1);
+            REQUIRE(gui.CastsTo<A::UI::System>(1));
+            REQUIRE(gui.IsSparse());
          }
       #endif
-
-         WHEN("The GUI system is created via abstractions") {
-            auto gui = root.CreateUnit<A::UI::System>();
-
-            // Update once                                              
-            root.Update(Time::zero());
-
-            THEN("Various traits change") {
-               root.DumpHierarchy();
-
-               REQUIRE(gui.GetCount() == 1);
-               REQUIRE(gui.CastsTo<A::UI::System>(1));
-               REQUIRE(gui.IsSparse());
-            }
-         }
 
          // Check for memory leaks after each cycle                     
          REQUIRE(memoryState.Assert());
